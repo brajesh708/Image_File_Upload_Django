e8678f86c67ddb43a262e3c985c34de87ad97c21
brajesh708 <brajeshmewada694@gmail.com>
2024-09-06 11:35:22 +0530

    VIEWS

--- project/add/views.py
+++ project/add/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render
 from .models import ItemInfo
-# from .forms import ItemInfoForm
+from .forms import ItemInfoForm
 
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html')
+    form = ItemInfoForm()
+    if request.method=="POST":
+        form = ItemInfoForm(request.POST,request.FILES)
+        if form.is_vaild():
+            form.save()
+    
+    data = ItemInfo.objects.all()
+    return render(request,"home.html",{'form':form,'data':data})
+
+
+def cart(request):
+    print(request.POST)
+    
+    
+def addtocart(request):
+    print(request.POST)
